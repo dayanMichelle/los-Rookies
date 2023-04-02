d261e4e883430f03157f6352d81a6dcbb52678fe
Dayan <dayaara6@gmail.com>
2023-04-02 10:19:00 -0400

    feat: añado la lista de cards y el component cards

--- src/app/votacion/[slug]/page.tsx
+++ src/app/votacion/[slug]/page.tsx
@@ -1,5 +1,11 @@
+import Card from "@/components/card/Card";
+import CardList from "@/components/cardList/CardList";
 import React from "react";
 
 export default function page() {
-  return <div>slug</div>;
+  return (
+    <>
+      <CardList />
+    </>
+  );
 }
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./Card.module.css";
+
+type CardProps = {
+  id: number;
+  instaName: string;
+  photo: string;
+  handleClick: (id: number) => void;
+  select: number;
+};
+const Card = ({
+  select,
+  id,
+  instaName = "@lola_funny",
+  photo = "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg",
+  handleClick,
+}: CardProps) => {
+  const styleImage = select === id ? { border: "2px blue solid" } : {};
+  return (
+    <div className={styles.card}>
+      <h4>{instaName}</h4>
+
+      <Image
+        style={styleImage}
+        onClick={() => handleClick(id)}
+        className={styles.img}
+        width={250}
+        height={200}
+        src={photo}
+        alt={`photo de ${instaName}`}
+      />
+    </div>
+  );
+};
+
+export default Card;
